f1dd00768061fd1ecd3d3fa872a975c4f6c1e431
Huseyin <huseyingunes@gmail.com>
2019-02-03 22:07:39 +0200

    Add files via upload

---
+++ problem_001.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/*
+https://projecteuler.net/problem=1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+--------------------------------------------------------
+10'un altýndaki 3 veya 5'in katlarý olan tüm doðal sayýlarý listelersek, 3, 5, 6 ve 9 sayýsýný elde ederiz.
+1000'in altýndaki 3 veya 5'in katlarýnýn toplamýný bulun.
+*/
+using namespace std;
+
+int main()
+{
+    unsigned long int toplam = 0;
+    for (int i=0; i<1000; i++)
+        if ((i%3==0) || (i%5==0))
+            toplam+=i;
+    cout << "Toplam : " << toplam << endl;
+    return 0;
+}
