a215005ccc6f3a3e8def0b0e62b35bda13b27916
Huseyin <huseyingunes@gmail.com>
2019-02-06 10:50:01 +0200

    Add files via upload

---
+++ problem_005.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+/*
+https://projecteuler.net/problem=5
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+----------------------------------
+2520 1'den 10'a kadar bütün sayýlara tam bölünebilen en küçük sayýdýr.
+O halde 1'den 20'ye kadar bütün sayýlara tam bölünebilen en küçük sayý kaçtýr?
+*/
+using namespace std;
+bool bolunuyor_mu(int sayi)
+{
+    for(int i=2;i<=20;i++)
+        if(sayi%i!=0)
+            return false;
+    return true;
+}
+
+int main()
+{
+    int s;
+    for(s=20;;s++)
+    {
+        if(bolunuyor_mu(s))
+            break;
+    }
+    cout << "Sayi : " << s << endl;
+    return 0;
+}
